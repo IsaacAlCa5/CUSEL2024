34e571fdcf8f73bdf21935a3d35e4dee8bf5b82b
isaac <isaac@DESKTOP-L8HRD22>
2024-07-02 00:23:18 -0600

    login isaac

--- curso/src/test/java/pruebaNG_Casos_de_prueba/selTest.java
+++ curso/src/test/java/pruebaNG_Casos_de_prueba/selTest.java
@@ -2,13 +2,25 @@ package pruebaNG_Casos_de_prueba;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class selTest {
   @Test
   public void test01() {
 	  WebDriver driver = new ChromeDriver();
-	  driver.get("https://www.google.com");
+	  driver.get("https://www.saucedemo.com/");
 	  System.out.print("hola");
+	 //Identificar los objetos GUIE/WebElements de la pagina
+	  WebElement nameTxt = driver.findElement(By.id("user-name"));
+	  WebElement pwdTxt= driver.findElement(By.name("password"));
+	  WebElement loginBotton = driver.findElement(By.id("login-button"));
+	  
+	  //Login
+	  nameTxt.sendKeys("Standard_user");
+	  pwdTxt.sendKeys("secret_sauce");
+	  loginBotton.click();
+	  
   }
 }
